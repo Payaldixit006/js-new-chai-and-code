330ec2da812a117325abd6fadfd4179f43c53485
Payaldixit006 <dixitpayal442@gmail.com>
2026-01-21 11:07:13 +0000

    comparisions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=0);
+// console.log(2<=5);
+// console.log(4!=3);
+// console.log(2==2);
+// console.log("2">1);
+// console.log("02">1);// USE AS NO
+console.log(null==0);//false
+console.log(null>0);//false 
+console.log(null>=0);//true
+//convert number to a number, treating as a zero
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined==0);avoid this type of comparision
+//=== strict means check integer also 
+console.log("2"==2);
+// datatype check
+//NaN > 0(undefined ko nan no not defined ye hamesha false deta )
+
